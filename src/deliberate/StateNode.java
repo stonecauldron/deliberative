994132baca5a9d48888ba3e574fe7aa6f6dd4a4d
Pedro Caldeira <pedro.caldeira@bluewin.ch>
2016-10-19 18:56:39 +0200

    Change definition of Move action.

---
+++ src/deliberate/StateNode.java
@@ -0,0 +1,9 @@
+package deliberate;
+
+import logist.task.TaskSet;
+import logist.topology.Topology.City;
+
+class State {
+    private City currentCity;
+    private TaskSet avalaibleTasks, takenTasks;
+}
